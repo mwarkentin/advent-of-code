c86b8cab37def25941a2082a207392a0a22c84ae
Michael Warkentin <mwarkentin@hey.com>
2021-12-04 13:11:53 -0500

    add day2 problem1 script and tests

---
+++ 2021/day-2/p1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Define a filename.
+filename = "input.txt"
+
+position = {}
+position["horizontal"] = 0
+position["depth"] = 0
+
+# 0: direction
+# 1: multiplier for direction
+cmd_list = {}
+cmd_list["up"] = ["depth", -1]
+cmd_list["down"] = ["depth", 1]
+cmd_list["backward"] = ["horizontal", -1]
+cmd_list["forward"] = ["horizontal", 1]
+
+def parse_cmd(cmd):
+    direction = cmd.split(" ")[0]
+    amount = int(cmd.split(" ")[1])
+
+    selected_cmd = cmd_list[direction]
+
+    return [selected_cmd[0], amount * selected_cmd[1]]
+
+
+with open(filename) as f:
+    cmds = f.readlines()
+
+for cmd in cmds:
+    vector = parse_cmd(cmd)
+    print(vector)
+    position[vector[0]] += vector[1]
+    print(position)
+
+print("final position multiplication", position["horizontal"] * position["depth"])
