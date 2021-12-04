82e9fabbddb0d213d63f366c8b7d1b26f54a70c9
Michael Warkentin <mwarkentin@hey.com>
2021-12-04 11:49:59 -0500

    add test for count_depth_changes func

---
+++ 2021/day-1/test_p1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import unittest
+
+from p1 import count_depth_changes
+
+
+class TestCountDepthChanges(unittest.TestCase):
+    def test_count_depth_changes(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        expected_result = {'decreased': 2, 'increased': 7, 'no_change': 0}
+        result = count_depth_changes(data)
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
