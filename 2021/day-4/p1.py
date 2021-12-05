41383d1777c562a0595d9c4e0420a07bce9b329e
Michael Warkentin <mwarkentin@hey.com>
2021-12-04 22:24:15 -0500

    parse boards

--- 2021/day-4/p1.py
+++ 2021/day-4/p1.py
@@ -1,7 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from pprint import pprint
 
 
 def parse_input(input):
     draws = [int(x) for x in input[0].split(",")]
 
-    return draws
+    boards = []
+    num_boards = (len(input) - 2) // 5
+    print(f"number of boards: {num_boards}")
+
+    for i in range(num_boards):
+        start_row = 2 + 6 * i
+        end_row = start_row + 5
+        boards.append([row.strip().split() for row in input[start_row:end_row]])
+
+    print("boards:")
+    pprint(boards)
+
+    return draws, boards
