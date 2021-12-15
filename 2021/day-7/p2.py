2c4974e35ffe659670f6796a48e21c607409d3c8
Michael Warkentin <mwarkentin@hey.com>
2021-12-15 13:18:18 -0500

    add d7p2 solution and tests

---
+++ 2021/day-7/p2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+
+
+def parse_input(input):
+    crabs = sorted([int(x) for x in input[0].split(",")])
+    crab_buckets = {}
+    max_x = crabs[-1] + 1
+
+    for x in range(max_x):
+        crab_buckets[x] = 0
+
+    for crab in crabs:
+        crab_buckets[crab] += 1
+
+    return crab_buckets, max_x
+
+def calc_fuel_to_position(crabs, position):
+    fuel_cost = 0
+    for x, num in crabs.items():
+        x_diff = sum(range(1, abs(x - position) + 1))
+        fuel_cost += x_diff * num
+
+    return fuel_cost
+
+def find_lowest_fuel_cost(crabs, max_x):
+    fuel_costs = []
+    for x in range(max_x):
+        fuel_costs.append(calc_fuel_to_position(crabs=crabs, position=x))
+
+    return min(fuel_costs)
---
+++ 2021/day-7/test_p2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from p2 import (
+    parse_input,
+    calc_fuel_to_position,
+    find_lowest_fuel_cost,
+)
+
+
+class TestProblemOne(unittest.TestCase):
+    def test_parse_intput(self):
+        filename = "input-sample.txt"
+        with open(filename) as f:
+            input = f.readlines()
+
+        expected_buckets = {0: 1, 1: 2, 2: 3, 3: 0, 4: 1, 5: 0, 6: 0, 7: 1, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 1, 15: 0, 16: 1}
+        crab_buckets, _ = parse_input(input=input)
+        self.assertEqual(crab_buckets, expected_buckets)
+
+    def test_calc_fuel_to_position(self):
+        filename = "input-sample.txt"
+        with open(filename) as f:
+            input = f.readlines()
+
+        crab_buckets, _ = parse_input(input=input)
+        self.assertEqual(calc_fuel_to_position(crabs=crab_buckets, position=2), 206)
+        self.assertEqual(calc_fuel_to_position(crabs=crab_buckets, position=5), 168)
+
+    def test_find_lowest_fuel_cost_sample(self):
+        filename = "input-sample.txt"
+        with open(filename) as f:
+            input = f.readlines()
+
+        crab_buckets, max_x = parse_input(input=input)
+        self.assertEqual(find_lowest_fuel_cost(crabs=crab_buckets, max_x=max_x), 168)
+
+    def test_find_lowest_fuel_cost_full(self):
+        filename = "input.txt"
+        with open(filename) as f:
+            input = f.readlines()
+
+        crab_buckets, max_x = parse_input(input=input)
+        self.assertEqual(find_lowest_fuel_cost(crabs=crab_buckets, max_x=max_x), 94813675)
+
+
+if __name__ == "__main__":
+    unittest.main()
